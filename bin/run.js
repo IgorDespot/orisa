097812133d4b74d87fd1df93edd88c30f4f1e799
IgorDespot <igordesspot@gmail.com>
2018-11-06 01:26:51 -0800

    Add configuration infrastructure

--- bin/run.js
+++ bin/run.js
@@ -1,13 +1,15 @@
 'use strict';
 
+const config = require('../config');
+
 const slackClient = require('../server/slackClient');
 const service = require('../server/service');
 const http = require('http');
 
 const server = http.createServer(service);
 
-const wit_token = 'GLO2LOJK2GHGDLLTY4B3LWXIOWZIWYV6'
-const slack_token = 'xoxb-467393544166-465993206002-fGNCMPoSsnoTj8XLHP1lEZ3Q';
+const wit_token = config.witToken;
+const slack_token = config.slackToken;
 const slackLogLevel = 'info';
 
 const witClient = require('../server/witClient')(wit_token);
